0703af20d9a78a8db7650a0770c03ed34ae4d863
Артамонов Александр <aartamonov@stech.ru>
2020-11-24 20:32:50 +0300

    RussianNumber.java JavaDoc style comments NumeralLevenshtein.java убрал комментарий к конструктору

--- src/main/java/number/utils/RussianNumber.java
+++ src/main/java/number/utils/RussianNumber.java
@@ -2,12 +2,7 @@ package number.utils;
 
 import number.utils.beans.RussianNumberTokens;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.LinkedHashSet;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class RussianNumber {
 
